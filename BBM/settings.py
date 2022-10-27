ab43d2ed08ce20c41568f3c147d82bce04c32c24
Nityananda Behera <nityanandabehera85132@gmail.com>
2022-10-27 20:44:06 +0530

    setting changes

--- BBM/settings.py
+++ BBM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fq6oe+6leko_$!&np+omm71@evf8rl4@x-g_28rc_8-!nw&&5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
